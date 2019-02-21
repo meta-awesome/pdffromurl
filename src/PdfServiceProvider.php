9fe6e2c17da64a5f7f4872a4cf0406b282f507d4
Adriano Zavareze Righi <adriano.righi@meta.com.br>
2019-02-21 14:56:54 -0300

    Create service provider

---
+++ src/PdfServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Metawesome\PdfFromUrl;
+
+use Illuminate\Support\ServiceProvider;
+
+class PdfFromUrlServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap de application services
+     *
+     * @return void
+     */
+    public function boot(){}
+}
